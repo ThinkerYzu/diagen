683547c5f7f4a8ae071dbc9bc755605d8202c2de
Thinker Lee <thinker.li@gmail.com>
2022-01-09 20:09:19 -0800

    Move package to the root

--- diagen/__init__.py
+++ diagen/__init__.py
@@ -4,6 +4,11 @@ class Module(object):
         self.uses = set()
         self.owns = set()
         self.calls = dict()
+        self.inherits = set()
+        pass
+
+    def inherit(self, other):
+        self.inherits.add(other)
         pass
 
     def use(self, other):
@@ -35,6 +40,9 @@ def draw(modules):
         for call in module.calls.keys():
             args = module.calls[call]
             print(f'    {module.name} -> {call.name} [label="{", ".join(args)}"]')
+            pass
+        for inherit in module.inherits:
+            print(f'    {inherit.name} -> {module.name} [arrowtail=empty, dir=back, color=blue, weight=10]')
             pass
         pass
     print('}')
